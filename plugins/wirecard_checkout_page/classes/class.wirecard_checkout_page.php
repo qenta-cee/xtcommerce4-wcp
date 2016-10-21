369e10a6d0ff18c6d51e788c29fabb134f748d37
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2016-10-21 12:22:15 +0200

    updated version number to 1.4.5

--- plugins/wirecard_checkout_page/classes/class.wirecard_checkout_page.php
+++ plugins/wirecard_checkout_page/classes/class.wirecard_checkout_page.php
@@ -42,7 +42,7 @@ class wirecard_checkout_page
     var $initPort = '443';
     var $initParams = array();
 
-    var $version = '1.4.4';
+    var $version = '1.4.5';
 
     var $paymentTypes = array(
         'WIRECARD_CHECKOUT_PAGE_SELECT' => 'SELECT',
