ab4d9945a6b91ac66cba1550ef4f804ddea9656b
Jacqueline Rinnhofer <jacqueline.rinnhofer@partner.wirecard.com>
2016-11-17 10:48:58 +0100

    #3 Update terms of use header

--- plugins/wirecard_checkout_page/classes/class.wirecard_checkout_page.php
+++ plugins/wirecard_checkout_page/classes/class.wirecard_checkout_page.php
@@ -1,25 +1,37 @@
 <?php
 /**
- * Shop System Plugins - Terms of use
+ * Shop System Plugins - Terms of Use
  *
- * This terms of use regulates warranty and liability between Wirecard
- * Central Eastern Europe (subsequently referred to as WDCEE) and it's
- * contractual partners (subsequently referred to as customer or customers)
- * which are related to the use of plugins provided by WDCEE.
+ * The plugins offered are provided free of charge by Wirecard Central Eastern
+ * Europe GmbH
+ * (abbreviated to Wirecard CEE) and are explicitly not part of the Wirecard
+ * CEE range of products and services.
  *
- * The Plugin is provided by WDCEE free of charge for it's customers and
- * must be used for the purpose of WDCEE's payment platform integration
- * only. It explicitly is not part of the general contract between WDCEE
- * and it's customer. The plugin has successfully been tested under
- * specific circumstances which are defined as the shopsystem's standard
- * configuration (vendor's delivery state). The Customer is responsible for
- * testing the plugin's functionality before putting it into production
- * enviroment.
- * The customer uses the plugin at own risk. WDCEE does not guarantee it's
- * full functionality neither does WDCEE assume liability for any
- * disadvantage related to the use of this plugin. By installing the plugin
- * into the shopsystem the customer agrees to the terms of use. Please do
- * not use this plugin if you do not agree to the terms of use!
+ * They have been tested and approved for full functionality in the standard
+ * configuration
+ * (status on delivery) of the corresponding shop system. They are under
+ * General Public License Version 2 (GPLv2) and can be used, developed and
+ * passed on to third parties under the same terms.
+ *
+ * However, Wirecard CEE does not provide any guarantee or accept any liability
+ * for any errors occurring when used in an enhanced, customized shop system
+ * configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and
+ * requires a comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard CEE does not guarantee
+ * their full functionality neither does Wirecard CEE assume liability for any
+ * disadvantages related to the use of the plugins. Additionally, Wirecard CEE
+ * does not guarantee the full functionality for customized shop systems or
+ * installed plugins of other vendors of plugins within the same shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before
+ * starting productive operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these
+ * terms of use. Please do not use the plugin if you do not agree to these
+ * terms of use!
  */
 
 defined('_VALID_CALL') or die ('Direct Access is not allowed.');
