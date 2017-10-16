db4fa0ffb76a598aec0a3860236a7985c92ffce9
tomazpu <32056440+tomazpu@users.noreply.github.com>
2017-10-16 09:12:39 +0200

    #15 Version increase

--- plugins/wirecard_checkout_page/classes/class.wirecard_checkout_page.php
+++ plugins/wirecard_checkout_page/classes/class.wirecard_checkout_page.php
@@ -57,7 +57,7 @@ class wirecard_checkout_page
     var $initPort = '443';
     var $initParams = array();
 
-    var $version = '1.6.2';
+    var $version = '1.6.3';
 
     var $paymentTypes = array(
         'WIRECARD_CHECKOUT_PAGE_SELECT' => 'SELECT',
